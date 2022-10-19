57e814c84d365779cab9967e95f4e0b7eee1aad8
nauticuff <felipecarlos200x@gmail.com>
2022-10-19 15:40:15 -0700

    changed code explanation

--- Program.cs
+++ Program.cs
@@ -1,14 +1,8 @@
 ﻿/*
-Carlos Felipe Sales
-9-16-22
+Carlos Felipe 
 Mini Challenge #7 - Reverse It
-This code takes in a user's input and displays it. It then 
-reverses the order of the input and displays the reverse. 
-Top code works ONLY for integers values
-and
-bottom code works ONLY for string values
-Peer review: Jeremy
-READY TO GO.
+10/19/22
+This code takes in a user's input and displays it. It then reverses the order of the input and displays the reverse.
 */
 
 Console.Clear();
